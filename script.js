d86ef4d5d915d0346a953bc947e499ee6b210b99
Pedro Ricardo Oliveira Córdova <43557558+prcordova@users.noreply.github.com>
2021-08-03 10:37:22 -0300

    simple page with day hour and background changes

---
+++ script.js
@@ -0,0 +1,27 @@
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+ var hora = data.getHours()
+
+ 
+
+if(hora >= 6 && hora < 12){
+    msg.innerHTML = `Bom dia, agora são ${hora} horas.`
+    // Bom dia!
+    img.src = `fotomanha.png`
+    document.body.style.background = '#eff0ef'
+
+} else if(hora >= 12 && hora <= 18) {
+    msg.innerHTML = `Boa tarde, agora são ${hora} horas.`
+    // boa tarde
+    img.src = `fototarde.png`
+    document.body.style.background = '#c3a08a'
+} else {
+    msg.innerHTML = `Boa noite, agora são ${hora} horas.`
+    // boa noite
+    img.src = `fotonoite.png` 
+    document.body.style.background = '#083641'
+}
+
+}
